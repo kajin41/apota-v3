d92a1250e6a0573c4eee7eee0a8b4ff3175591ee
greg <kajin41@gmail.com>
2016-09-18 18:36:52 -0400

    fixed the routing problem

--- APP/__init__.py
+++ APP/__init__.py
@@ -93,7 +93,7 @@ except FileExistsError:
 UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__)) + '/temp'
 TEMP_MEDIA_FOLDER = os.path.dirname(os.path.realpath(__file__)) + '/temp'
 
-from APP.views.backend import *
+from APP.views.backend import admin, errors, events, families, files, github, inventory, login, semesters, upload, users
 
 
 # --- For Analytics
